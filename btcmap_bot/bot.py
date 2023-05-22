d987d329a2b2ceb2b4438c0587dbaec3db031ba3
Marcel Hernandez <e6990620@gmail.com>
2023-05-22 19:58:59 +0200

    comprehensiocalypse

--- btcmap_bot/bot.py
+++ btcmap_bot/bot.py
@@ -28,23 +28,22 @@ def run(community_name):
 
     community_geo_json = shapely.geometry.shape(community['tags']['geo_json'])
 
-    new_events = json.loads(requests.get('https://api.btcmap.org/v2/events?updated_since=2023-05-19').content)
-
-    # TODO also filter by last execution time
-    relevant_events = [
-        event
-        for event in new_events
-        if event['type'] == 'create' and event['element_id'].startswith('node:')
-    ]
-
-    new_businesses = [
-        json.loads(requests.get(f'https://api.btcmap.org/v2/elements/{event["element_id"]}').content)
-        for event in relevant_events
-    ]
-
-    new_local_businesses = [
-        business
-        for business in new_businesses
+    nostr_messages = [
+        'A new business accepting Bitcoin in {}! {} https://btcmap.org/merchant/{}'.format(
+            community['tags']['name'],
+            business['osm_json']['tags']['name'],
+            business['id']
+        )
+        for business in [
+            json.loads(requests.get(f'https://api.btcmap.org/v2/elements/{event["element_id"]}').content)
+            for event in [
+                event
+                for event in
+                json.loads(requests.get('https://api.btcmap.org/v2/events?updated_since=2023-05-19').content)
+                if event['type'] == 'create' and event['element_id'].startswith('node:')
+                # TODO also filter by last execution time
+            ]
+        ]
         if shapely.contains(
             community_geo_json,
             shapely.geometry.Point(business['osm_json']['lon'], business['osm_json']['lat'])
@@ -52,18 +51,9 @@ def run(community_name):
     ]
 
     print('Found {} new local businesses in {} since whatever'.format(
-        len(new_local_businesses),
+        len(nostr_messages),
         community['tags']['name'])
     )
-
-    nostr_messages = [
-        'A new business accepting Bitcoin in {}! {} https://btcmap.org/merchant/{}'.format(
-            community['tags']['name'],
-            business['osm_json']['tags']['name'],
-            business['id']
-        )
-        for business in new_local_businesses
-    ]
 
     # TODO supress stdout
     for message in nostr_messages:
