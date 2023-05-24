af79b86ad870864db726cd929d92979cfe9057a0
Marcel Hernandez <e6990620@gmail.com>
2023-05-24 19:14:30 +0200

    A few refactors

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, time
 from datetime import timezone
 from pathlib import Path
 import json
@@ -7,8 +7,10 @@ import shapely.geometry
 import subprocess
 import sys
 
-FUCKY_FORMAT = '%Y-%m-%dT%H:%M:%S.%fZ'
-LAST_EXEC_FILEPATH = str(Path(__file__).parent) + '/.last_execution_time'
+LAST_EXEC_DATETIME_FORMAT = '%Y-%m-%dT%H:%M:%S.%fZ'
+LAST_EXEC_FILEPATH = Path(__file__).parent / '.last_execution_time'
+BTCMAP_ROOT_URL = "https://api.btcmap.org"
+BTCMAP_API_VERSION = "v2"
 
 
 def invoke_noscl(message: str) -> int:
@@ -17,9 +19,12 @@ def invoke_noscl(message: str) -> int:
 
 def main(community_name: str) -> None:
     execution_time = datetime.now(timezone.utc)
-    last_execution_time = datetime.strptime(Path(LAST_EXEC_FILEPATH).read_text().strip(), FUCKY_FORMAT)
+    try:
+        last_execution_time = datetime.strptime(LAST_EXEC_FILEPATH.read_text().strip(), LAST_EXEC_DATETIME_FORMAT)
+    except FileNotFoundError:
+        last_execution_time = datetime.combine(date=datetime.today(), time=time(hour=0, minute=0, second=0))
 
-    community = requests.get(f'https://api.btcmap.org/v2/areas/{community_name}')
+    community = requests.get(f'{BTCMAP_ROOT_URL}/{BTCMAP_API_VERSION}/areas/{community_name}')
 
     if community.status_code != 200:
         print(f'Community {community_name}  does not exist')
@@ -40,15 +45,15 @@ def main(community_name: str) -> None:
             business['id']
         )
         for business in [
-            json.loads(requests.get(f'https://api.btcmap.org/v2/elements/{event["element_id"]}').content)
+            json.loads(requests.get(f'{BTCMAP_ROOT_URL}/{BTCMAP_API_VERSION}/elements/{event["element_id"]}').content)
             for event in [
                 event
                 for event in json.loads(requests.get(
-                    'https://api.btcmap.org/v2/events?updated_since=' + last_execution_time.strftime('%Y-%m-%d')
+                    f"{BTCMAP_ROOT_URL}/{BTCMAP_API_VERSION}/events?updated_since={last_execution_time.strftime('%Y-%m-%d')}"
                 ).content)
                 if event['type'] == 'create' and
                 event['element_id'].startswith('node:') and
-                last_execution_time < datetime.strptime(event['created_at'], FUCKY_FORMAT)
+                last_execution_time < datetime.strptime(event['created_at'], LAST_EXEC_DATETIME_FORMAT)
             ]
         ]
         if shapely.contains(
@@ -57,9 +62,11 @@ def main(community_name: str) -> None:
         )
     ]
 
-    print('Found {} new local businesses in {} since whatever'.format(
+    print('Found {} new local businesses in {} since {}'.format(
         len(nostr_messages),
-        community['tags']['name'])
+        community['tags']['name'],
+        last_execution_time.isoformat()
+    )
     )
 
     save_state = True
@@ -73,7 +80,7 @@ def main(community_name: str) -> None:
 
     if save_state:
         with open(LAST_EXEC_FILEPATH, 'w') as fp:
-            fp.write(execution_time.strftime(FUCKY_FORMAT))
+            fp.write(execution_time.strftime(LAST_EXEC_DATETIME_FORMAT))
 
 
 if __name__ == "__main__":
